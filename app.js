d784baa579afbb97744925c1bde0e4befb0a5e0f
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-02-14 22:42:22 -0500

    server setup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+
+// route 
+app.get('/hello', (req, res) => {
+    res.send('Task manager App');
+})
+const port = 3000;
+
+app.listen(port, console.log(`server is listening on the port ${port}`));
