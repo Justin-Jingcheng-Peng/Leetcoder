367d3bf71bedff73440df1563dcee5b5e8a1f331
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-02-15 01:56:42 -0500

    all basic set

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-require('./db/connect');
+
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
-
+const connectDB = require('./db/connect');
+require('dotenv').config()
 // mid ware
 
 app.use(express.json());
@@ -14,5 +15,14 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks);
 const port = 3000;
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on the port ${port}`));
+    } catch(error){
+        console.log(error);
+    }
+}
 
-app.listen(port, console.log(`server is listening on the port ${port}`));
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://JustinP:Pjc031209!@newproject.jbhzg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-mongoose
-    .connect(connectionString, {
+
+const connectDB = (url) => {
+    return mongoose
+    .connect(url, {
         useNewUrlParser:true,
         useCreateIndex:true,
         useFindAndModify:false,
         useUnifiedTopology:true,
     })
-    .then(() => console.log('Connected to the DB'))
-    .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
